291900d7374afce085341fb3237c6ba1957e3231
harut0601 <sahakyanharut06@gmail.com>
2019-10-20 14:10:28 +0400

    week1

---
+++ week1/Problem1.py
@@ -0,0 +1,17 @@
+project = "cake"
+difficulty = int(input("Difficulty(1-5):"))
+ingredients = ["flour","butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+print("Apple is in ingredients:", "apple" in ingredients)
+print("Butter is in ingredients:", "butter" in ingredients)
+print("Either eggs or margarine:", "eggs or margarine" in ingredients)
+print("Eggs and margarine:", "eggs and margarine" in ingredients)
+flour = 175
+butter = 175
+sugar = 100
+cocoa_powder = 2
+baking_powder = 0.5
+print("Flour:", flour, "g")
+print("butter:", butter, "g")
+print("Sugar", sugar, "g")
+print("Cocoa Powder", cocoa_powder, "ts")
+print("Baking Powder", baking_powder, "ts")
