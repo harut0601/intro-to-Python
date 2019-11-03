6e75a127cbfce64fd673dbc23d21fcd182d1922f
harut0601 <56783287+harut0601@users.noreply.github.com>
2019-11-03 13:52:12 +0400

    week3

--- week3/Homework/problem1.py
+++ week3/Homework/problem1.py
@@ -1,11 +1,11 @@
 #Problem1
 a = [1, 4, 5, 7, 8, -2, 0, -1]
-
 #Problem2
 print(a[3], a[5])
 
 #Problem3
-a_sorted = a[::-1]
+a_sorted = a.copy()
+a_sorted.sort(reverse=True)
 print(a_sorted)
 
 #Problem4
@@ -13,4 +13,20 @@ print(a_sorted[0:3])
 print(a_sorted[2:6])
 
 #Problem5
-a_sorted.remove(a[2])
+print(a_sorted)
+a_sorted.pop(3)
+a_sorted.pop(2)
+print(a_sorted)
+
+#Problem6
+b = ["grapes", "Potatoes","tomatoes", "Orange", "Lemon", "Broccoli", "Carrot", "Sausages"]
+
+#Problem7
+b_sorted = b.copy()
+b_sorted.sort()
+print(b_sorted)
+
+#Problem8
+c_first = a[0:2]
+c_second = b[3:6]
+print(c_first + c_second)
